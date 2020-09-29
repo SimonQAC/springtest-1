0351cc17169f3c120883dee605ed45e08a915048
Simon Peppitt <simonworkpersonal@gmail.com>
2020-09-29 10:22:03 +0100

    first commit

---
+++ src/test/java/com/qa/springtest/Springtest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
